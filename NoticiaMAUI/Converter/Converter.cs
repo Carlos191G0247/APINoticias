29b4de96a47aec4d056f57523f4b6fffeb040ad6
CesarRMZ-01 <alejandrores4@hotmail.com>
2023-06-08 11:51:18 -0600

    ya jala mostrar imagen

---
+++ NoticiaMAUI/Converter/Converter.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Globalization;
+using System.IO;
+using System.Text.RegularExpressions;
+using Microsoft.Maui.Controls;
+using Microsoft.Maui.Controls.Xaml;
+
+namespace NoticiaMAUI.Converter
+{
+    public class Converter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is string base64String)
+            {
+                // Eliminar espacios en blanco adicionales si es necesario
+                base64String = base64String.Trim();
+
+                // Verificar si la cadena es válida antes de convertirla
+                if (base64String.Length % 4 == 0 && Regex.IsMatch(base64String, @"^[a-zA-Z0-9+/]*={0,2}$"))
+                {
+                    byte[] imageBytes = System.Convert.FromBase64String(base64String);
+                    ImageSource imageSource = ImageSource.FromStream(() => new MemoryStream(imageBytes));
+                    return imageSource;
+                }
+                else
+                {
+                    // La cadena Base64 no es válida
+                    // Maneja el error o devuelve un valor predeterminado
+                }
+            }
+
+            return null;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
+
+
--- NoticiaMAUI/ViewModels/ViewModel.cs
+++ NoticiaMAUI/ViewModels/ViewModel.cs
@@ -12,25 +12,18 @@ using Microsoft.Maui.Controls;
 
 namespace NoticiaMAUI.ViewModels
 {
-    public class ViewModel: INotifyPropertyChanged
+    public class ViewModel : INotifyPropertyChanged
     {
         public Command VerInicioSesionView { get; set; }
-
         public Command VerAgregarNoticiaView { get; set; }
         public ICommand SeleccionarImagenCommand { get; }
+        public Command AgregarNotciaCommand { get; set; }
 
-        public Command AgregarNotciaCommand{get; set; }
-
-        readonly NoticiaService  noticiaserver = new(
-            
-
-            );
+        readonly NoticiaService noticiaserver = new NoticiaService();
 
         private string _imagePath;
         public Noticia noticiass { get; set; } = new Noticia();
-
         public ObservableCollection<Noticia> NoticiaList { get; set; } = new ObservableCollection<Noticia>();
-
 
         public string ImagePath
         {
@@ -44,18 +37,18 @@ namespace NoticiaMAUI.ViewModels
                 }
             }
         }
+
         public ViewModel()
         {
-            //Para todos
+            // Para todos
             VerInicioSesionView = new Command(VerSesion);
-            //Para Reporteros
+            // Para Reporteros
             VerAgregarNoticiaView = new Command(VerAgregarRep);
             AgregarNotciaCommand = new Command(AgregarNoticia);
-            //imagen
+            // Imagen
             SeleccionarImagenCommand = new Command(async () => await SeleccionarImagen());
 
-            cargarNoticias();
-
+            CargarNoticias();
         }
 
         private async Task SeleccionarImagen()
@@ -69,7 +62,6 @@ namespace NoticiaMAUI.ViewModels
 
                 if (result != null)
                 {
-          
                     ImagePath = result.FullPath;
                 }
             }
@@ -78,6 +70,7 @@ namespace NoticiaMAUI.ViewModels
                 // Manejar cualquier error al seleccionar la imagen
             }
         }
+
         public async Task CargarNoticias()
         {
             // Obtener la lista de noticias desde el servicio
@@ -92,43 +85,11 @@ namespace NoticiaMAUI.ViewModels
 
         public async void AgregarNoticia()
         {
-
             noticiass.Imagen = ConvertImageToBase64(ImagePath);
 
             NoticiaList.Add(noticiass);
             await noticiaserver.Insert(noticiass);
-            //// Crear una instancia de Noticia con los datos ingresados
-            //Noticia nuevaNoticia = new Noticia
-            //{
-            //    Titulo = noticiass.Titulo,
-            //    Autor = noticiass.Autor,
-            //    Contenido = noticiass.Contenido,
-            //    Imagen = ConvertImageToBase64(ImagePath),
-            //    Fecha = DateTime.Now.Date
-            //};
-
-            //// Enviar la nueva noticia al servicio para agregarla a la base de datos
-            //Noticia noticiaAgregada = await noticiaserver.Insert(nuevaNoticia);
-
-            //// Si se agregó correctamente, agregarla a la lista local
-            //if (noticiaAgregada != null && noticiaAgregada.Id > 0)
-            //{
-            //    NoticiaList.Add(noticiaAgregada);
-            //}
-
-            //// Limpiar los campos y restablecer la imagen
-            //noticiass = new Noticia();
-            //ImagePath = string.Empty;
         }
-        async void cargarNoticias()
-        {
-            NoticiaList.Clear();
-            var datos = await noticiaserver.GetNoticias();
-            datos.ForEach(x => NoticiaList.Add(x));
-            Actualizar(nameof(NoticiaList));
-
-        }
-
 
         private async void VerAgregarRep()
         {
@@ -139,6 +100,7 @@ namespace NoticiaMAUI.ViewModels
         {
             await Shell.Current.GoToAsync("//LoginView");
         }
+
         private string ConvertImageToBase64(string imagePath)
         {
             if (string.IsNullOrEmpty(imagePath))
